528a91e64ceaf8a5f4d46b5f5fa6157987ea022a
Mdigo12 <mdigojacob@gmail.com>
2020-08-05 13:45:27 +0300

    teams section with animations done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
 /* ============================
      preloader
 ============================== */
-$(window).on('load', function(){
+$(window).on('load', function () {
     $('#status').hide();
     $('#preloader').delay().hide();
 
 });
+
+/* ============================
+     Team
+============================== */
+
+$(function(){
+$("#team-members").owlCarousel({
+    items:2,
+    loop:true,
+    nav: true,
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    dots:false,
+    autoplay:true,
+    smartSpeed:700,
+    autoplayHoverPause:true,
+});
+});
+/* ============================
+     Progressbars
+============================== */
+$(function(){
+    $("#progress-elements").waypoint(function () {
+        $(".progress-bar").css(
+            "width",function(){return $(this).attr("aria-valuenow")+"%"});
+        this.destroy();
+        
+    }, {offset:"bottom-in-view"});
+});
+
+$(function(){
+        //  $(".progress-bar").css(
+        //      "width",function(){return $(this).attr("aria-valuenow")+"%"});
+
+    //  $(".progress-bar").each(function() {
+    //      $(this).animate(
+    //          {width: $(this).attr("aria-valuenow") + "%"},1000);
+    //  });
+});
+
+/* ============================
+    
+============================== */
